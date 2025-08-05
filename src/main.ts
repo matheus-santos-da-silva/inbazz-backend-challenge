8475e483df9aca805efa12ef55eb945d43a35dcf
matheus-santos-da-silva <maatheus.santos223@gmail.com>
2025-08-05 17:49:52 -0300

    initial-configs

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { AppModule } from "./app.module";
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle("Todo List")
+    .setDescription("Inbazz Backend Challenge - TODO List")
+    .setVersion("1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
