82a172745c5929f3bea642e16798ff6b5d80ebf3
matheus-santos-da-silva <maatheus.santos223@gmail.com>
2025-08-08 09:50:38 -0300

    feat: add class-transformer to exclude createdAt of create or update todo response

--- src/presentation/todos/view-model/todo-vm.ts
+++ src/presentation/todos/view-model/todo-vm.ts
@@ -1,22 +1,31 @@
 import { Status } from "@prisma/client";
 import { ApiProperty } from "@nestjs/swagger";
+import { Exclude, Expose } from "class-transformer";
 
 export class TodoResponseViewModel {
+  @Expose()
   @ApiProperty({ example: "999cc992-e792-4e1d-8f12-0fb51f06cfe2" })
   id: string;
 
+  @Expose()
   @ApiProperty({ example: "Fazer Planilha" })
   title: string;
 
+  @Expose()
   @ApiProperty({ example: "Terminar planilha de produtos que meu chefe pediu" })
   description: string;
 
+  @Expose()
   @ApiProperty({ example: Status.PENDING })
   status: Status;
 
-  @ApiProperty({ example: "999cc992-e792-4e1d-8f12-0fb51f06cfe2" })
+  @Expose()
+  @ApiProperty({
+    example: "999cc992-e792-4e1d-8f12-0fb51f06cfe2",
+    format: "uuid",
+  })
   categoryId: string;
 
-  @ApiProperty({ example: new Date() })
+  @Exclude()
   createdAt: Date;
 }
