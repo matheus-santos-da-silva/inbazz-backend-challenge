9d0bfe0f88a9090dcf226c34078039fde24a7cc3
Matheus Santos <132293166+matheus-santos-da-silva@users.noreply.github.com>
2025-08-13 18:15:44 -0300

    feat: add length validation in category-input

--- src/presentation/categories/dtos/category-input.dto.ts
+++ src/presentation/categories/dtos/category-input.dto.ts
@@ -1,14 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class CategoryInputDTO {
   @ApiProperty({ example: "Trabalho" })
   @IsNotEmpty({ message: "O nome da categoria é obrigatório!" })
   @IsString({ message: "O nome deve ser uma string!" })
+  @Length(3, 100, {
+    message: "O nome da categoria precisa ter entre 3 a 100 caracteres!",
+  })
   name: string;
 
   @ApiProperty({ example: "Categoria para as tarefas do meu trabalho" })
   @IsNotEmpty({ message: "A descrição da categoria é obrigatória!" })
   @IsString({ message: "A descrição deve ser uma string!" })
+  @Length(3, 255, {
+    message: "A descrição precisa ter entre 3 a 100 caracteres!",
+  })
   description: string;
 }
